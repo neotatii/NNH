45955fcd9fc3ec67d22a8a5117a4aca8f4d0cedb
[naoufal elhasyouty] <[naoufalelhasyouty@gmail.com]>
2022-05-26 19:21:31 +0100

    PROBLEM FIXED

--- PROJECTS/script.js
+++ PROJECTS/script.js
@@ -34,3 +34,12 @@ function closeModal(modal) {
   modal.classList.remove('active')
   overlay.classList.remove('active')
 }
+
+
+
+document.querySelector("#show-login").addEventListener("click",function(){
+  document.querySelector(".popup").classList.add("active");
+ });
+ document.querySelector(".popup .close-btn").addEventListener("click",function(){
+   document.querySelector(".popup").classList.remove("active");
+ });
