a144770f4749cd5f8ec18ce9ec3b457954f43ae3
[naoufal elhasyouty] <[naoufalelhasyouty@gmail.com]>
2022-06-08 02:15:52 +0100

    08/06/2022 2:15

--- CLIENT/edit-script.js
+++ CLIENT/edit-script.js
@@ -43,3 +43,8 @@ document.querySelector("#show-login").addEventListener("click",function(){
 document.querySelector(".popup .close-btn").addEventListener("click",function(){
   document.querySelector(".popup").classList.remove("active");
 });
+
+
+
+
+
