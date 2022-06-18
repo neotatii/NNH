7adc221014d3241baffb2caf652c9840da809cb6
maf29 <73944513+maf29@users.noreply.github.com>
2022-06-18 19:53:45 +0200

    changes: all working

---
+++ deleteClient.php
@@ -0,0 +1,44 @@
+<?php
+    include_once("config.php");
+    include_once("login.php");
+
+    session_start();
+
+    $dni = '';
+    $nombre = '';
+    $phone = '';
+    $email = '';
+    $sexo = '';
+
+    if(isset($_GET['delete'])){
+        $cliente = $_GET['delete'];
+        $connec->query("DELETE FROM practica.clientes WHERE dni = '$cliente'");
+        
+        $_SESSION['message'] = "Se ha borrado el Cliente $cliente !";
+        $_SESSION['msg_type'] = "eliminado";
+
+        header("location: clients.php");
+
+    }
+    if(isset($_GET['edit'])){
+        /*$cliente = $_GET['delete'];
+        $connec->query("DELETE FROM practica.clientes WHERE dni = '$cliente'");
+        
+        $_SESSION['message'] = "Se ha borrado el Cliente $cliente !";
+        $_SESSION['msg_type'] = "eliminado";
+
+        header("location: clients.php");
+*/
+        $cliente = $_GET['edit'];
+        $result = $connec->query("SELECT a.dni, a.nombre, a.phone, a.emapil, b.sexo FROM practica.personas AS a, practica.clientes AS b WHERE a.dni = '$cliente' AND b.dni = '$cliente'");
+        if(count($connec) ==1){
+            $row = $result->fetch_array();
+            $dni = $row['dni'];
+            $nombre = $row['nombre'];
+            $phone = $row['phone'];
+            $email = $row['email'];
+            $sexo = $row['sexo'];
+
+        }
+    }
+?>
